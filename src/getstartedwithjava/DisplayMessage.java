96756de8704c7bede448e9cfa9300ce3998e05a8
Rishab-Sharma-777 <sharmarishab777@gmail.com>
2024-04-14 02:12:23 +0530

    Update and Added More Files

---
+++ src/getstartedwithjava/DisplayMessage.java
@@ -0,0 +1,12 @@
+package getstartedwithjava;
+
+public class DisplayMessage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java course!");
+        System.out.println("Hello");
+        System.out.println("Ninja");
+	}
+
+}
